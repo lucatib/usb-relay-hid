e15aa67e2c1c6be15aa00332a4e0ee31588ddf52
luca tiburzio <luca.tiburzio@gmail.com>
2017-11-21 23:09:05 +0100

    Vb.net demo

---
+++ windows-vs/GUIapp/Form1.vb
@@ -0,0 +1,10 @@
+﻿Imports System.Runtime.InteropServices
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim mdevlist As IntPtr = usb_relay_device_enumerate()
+        Dim mdev As usb_relay_device_info = CType(Marshal.PtrToStructure(mdevlist, GetType(usb_relay_device_info)), usb_relay_device_info)
+        Dim mresult As Integer = usb_relay_device_open(mdev)
+
+    End Sub
+End Class
---
+++ windows-vs/GUIapp/hidRele.vb
@@ -0,0 +1,28 @@
+﻿Imports System.Runtime.InteropServices
+
+Module hidRele
+    <StructLayout(LayoutKind.Sequential, CharSet:=CharSet.Ansi, Pack:=1)>
+    Public Structure usb_relay_device_info
+        Public serial_number As String
+        Public device_path As String
+        Public type As UInt32
+        Public [Next] As IntPtr
+    End Structure
+
+    <DllImport("USB_RELAY_DEVICE.dll", SetLastError:=True)> Function usb_relay_device_enumerate _
+     () As IntPtr
+    End Function
+    <DllImport("USB_RELAY_DEVICE.dll", SetLastError:=True)> Function usb_relay_device_free_enumerate _
+     (ByRef DeviceInterfaceData As usb_relay_device_info) As Boolean
+    End Function
+    <DllImport("USB_RELAY_DEVICE.dll", SetLastError:=True)> Function usb_relay_device_open _
+     (ByRef device_info As usb_relay_device_info) As Integer
+    End Function
+
+    Public Enum UsbRelayDeviceType
+        OneChannel = 1
+        TwoChannel = 2
+        FourChannel = 4
+        EightChannel = 8
+    End Enum
+End Module
