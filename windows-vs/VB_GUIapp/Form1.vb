5f9834187d4a4c4a3f5e239f90dead56f77f36b4
luca tiburzio <luca.tiburzio@gmail.com>
2017-11-22 00:46:32 +0100

    Updated C# wrapper

---
+++ windows-vs/VB_GUIapp/Form1.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Runtime.InteropServices
+Imports UsbRelay
+
+Public Class Form1
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        RelayDeviceWrapper.usb_relay_init()
+        Dim minfo As usb_relay_device_info = RelayDeviceWrapper.usb_relay_device_enumerate()
+        Dim mhandle As Integer = RelayDeviceWrapper.usb_relay_device_open_with_serial_number(minfo.serial_number, minfo.serial_number.Length)
+        RelayDeviceWrapper.usb_relay_device_open_one_relay_channel(mhandle, 0)
+        RelayDeviceWrapper.usb_relay_device_open_one_relay_channel(mhandle, 1)
+        RelayDeviceWrapper.usb_relay_device_open_one_relay_channel(mhandle, 2)
+    End Sub
+End Class
